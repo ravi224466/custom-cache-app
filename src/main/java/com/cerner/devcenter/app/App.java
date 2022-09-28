ae08693fd79097cef45fa7c85a1787235e6185e4
ravi224466 <ravi224466@gmail.com>
2022-09-28 12:20:09 +0530

    modified App for updated sysout

--- src/main/java/com/cerner/devcenter/app/App.java
+++ src/main/java/com/cerner/devcenter/app/App.java
@@ -4,10 +4,11 @@ package com.cerner.devcenter.app;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Updated Hello World!" );
     }
 }
